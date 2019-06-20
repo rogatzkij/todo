4cebea008bee26ffa4f8c0f29611a0d5387aa1bc
Рогацкий андрей <rogatzkij@yandex.ru>
2019-06-20 17:45:48 +0300

    try ssh fix

--- images/init-letsencrypt.sh
+++ images/init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.com www.example.com)
+domains=(todotodo.ml www.todotodo.ml)
 rsa_key_size=4096
 data_path="./certbot"
-email="" # Adding a valid address is strongly recommended
+email="rogatzkij@yandex.ru" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
