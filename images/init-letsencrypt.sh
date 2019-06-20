797dd0b572139cebba66ba7b1a3bf2bb5022e4a2
Andrey Rogatzkij <rogatzkij@yandex.ru>
2019-06-20 15:11:47 +0000

    fix

--- images/init-letsencrypt.sh
+++ images/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(todotodo.ml www.todotodo.ml)
+domains=(todotodo.ml)
 rsa_key_size=4096
 data_path="./certbot"
 email="rogatzkij@yandex.ru" # Adding a valid address is strongly recommended
